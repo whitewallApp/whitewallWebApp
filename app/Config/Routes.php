6f08c0a90d0e21c8c857cc7782845a10d096eaa3
Thomas <thomas.ed.dick@gmail.com>
2023-01-20 16:28:28 -0700

    Add Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,7 @@ $routes = Services::routes();
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Login');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -29,7 +29,16 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+
+//Base URLs
+$routes->get('/', 'Login::index');
+$routes->get('/dashboard', 'Dashboard::index');
+$routes->get('/collections', 'Collection::index');
+$routes->get('/categories', 'Category::index');
+$routes->get('/account', 'Account::index');
+
+//Parameter URLs
+
 
 /*
  * --------------------------------------------------------------------
