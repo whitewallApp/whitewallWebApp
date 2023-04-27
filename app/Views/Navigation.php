29d5c43d961aba435b878c186590fd808be44925
Thomas Dick <thomas.ed.dick@gmail.com>
2023-04-27 13:14:37 -0600

    make it so dropdown working

--- app/Views/Navigation.php
+++ app/Views/Navigation.php
@@ -17,14 +17,16 @@
         <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
     </form>
 
-    <div class="dropleft dropdown ml-3 p-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <div class="dropleft dropdown ml-3 p-3">
         <?php if ($userIcon == "") : ?>
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="crimson" class="bi bi-person-circle dropdown-toggle" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="crimson" class="bi bi-person-circle dropdown-toggle" viewBox="0 0 16 16" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
             </svg>
         <?php else : ?>
-            <img class="profile-photo" src="<?= $userIcon ?>">
+            <div data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img class="profile-photo" src="<?= $userIcon ?>">
+            </div>
         <?php endif ?>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <div class="col">
