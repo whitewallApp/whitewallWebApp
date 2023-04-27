c43c4292e2789f79867c0a3af110a84c50b156b3
Thomas Dick <thomas.ed.dick@gmail.com>
2023-04-27 12:16:45 -0600

    redirect to dashboard if no permissions

--- app/Filters/UserValidation.php
+++ app/Filters/UserValidation.php
@@ -20,11 +20,12 @@ class UserValidation implements FilterInterface
             return redirect()->to("");
         }
 
-        $canView = $userModel->getPermissions($session->get("user_id"), $session->get("brand_name"), permissions: ["p_view"]);
-
-        if (!$canView[$page]["p_view"]){
-            $session->setFlashdata('prev_url', "dashboard");
-            return redirect()->to("");
+        if ($page != "dashboard"){
+            $canView = $userModel->getPermissions($session->get("user_id"), $session->get("brand_name"), permissions: ["p_view"]);
+            if (!$canView[$page]["p_view"]){
+                $session->setFlashdata('prev_url', "dashboard");
+                return redirect()->to("");
+            }
         }
 
     }
