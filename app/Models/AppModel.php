d3414702e815a54cad553cca67efdd5acfbab461
Thomas <thomas.ed.dick@gmail.com>
2023-07-28 13:41:30 -0600

    add docs

--- app/Models/AppModel.php
+++ app/Models/AppModel.php
@@ -17,14 +17,28 @@ class AppModel extends Model
     protected $createdField  = 'publishedDate';
     protected $updatedField  = 'dateUpdated';
 
-
+    /**
+     * This will update a row by something other than the primary id | primary id is default
+     *
+     * @param string $id | id
+     * @param array $data | data to update, is assoc
+     * @param string $updateBy | column to search for $id
+     * @return void
+     */
     public function updateBy($id, $data, $updateBy = "id")
     {
         $builder = $this->db->table("app");
         $builder->where($updateBy, $id);
         $builder->update($data);
     }
-
+    
+    /**
+     * Updates a row by multipul ids
+     *
+     * @param array $ids | assoc. array of ids 
+     * @param array $data | assoc. array of data
+     * @return void
+     */
     public function updateByMultipule($ids, $data)
     {
         $builder = $this->db->table("app");
@@ -34,6 +48,13 @@ class AppModel extends Model
         $builder->update($data);
     }
 
+    /**
+     * Selects columns of a row by multipule ids
+     *
+     * @param array|string $selections | the columns you want to get
+     * @param array $ids | assoc. array of ids
+     * @return array
+     */
     public function selectByMultipule($selections, $ids)
     {
         $builder = $this->db->table("app");
