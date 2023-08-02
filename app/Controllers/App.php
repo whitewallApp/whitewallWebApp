83e2190a1d5f8722aa5f38911af0f578557bed47
Thomas <thomas.ed.dick@gmail.com>
2023-08-02 13:03:40 -0600

    maybe?

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -111,11 +111,15 @@ class App extends BaseController
             } catch (\Throwable $e) {
             }
 
+            if (file_exists($brandingPath . "app-log.txt")){
+                unlink($brandingPath . "app-log.txt");
+            }
+
             //variables for command line
             $descriptorspec = array(
                 0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-                1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-                2 => array("pipe", "w") // stderr is a file to write to
+                1 => array("file", $brandingPath . "app-log.txt", "a"),  // stdout is a pipe that the child will write to
+                2 => array("file", $brandingPath . "app-log.txt", "a") // stderr is a file to write to
             );
 
 
